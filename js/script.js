bba50996ca889df09a7bc633b6377891f74b02eb
alonemanuel <alon@alonemanuel.com>
2023-01-25 12:33:43 +0200

    article is basically layed out.

--- js/script.js
+++ js/script.js
@@ -13,6 +13,29 @@ function customizeTheme() {
     // });
 }
 
+
+
+function shrinkToMinWidth() {
+    var d = document.querySelectorAll('.min-width'), i, w, width, height;
+    
+    for (i = 0; i < d.length; i++) {
+        width = d[i].offsetWidth;
+        height = d[i].offsetHeight;
+        
+        for (w = width; w; w--) {
+            d[i].style.width = w + 'px';
+            if (d[i].offsetHeight !== height)
+            break;
+        }
+        
+        if (w < d[i].scrollWidth) {
+            d[i].style.width = d[i].style.maxWidth = d[i].scrollWidth + 'px';
+        } else {
+            d[i].style.width = (w + 1) + 'px';
+        }
+    }
+}
+
 function main() {
     activateTooltipster();
     customizeTheme();
