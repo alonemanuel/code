ffb7836a75782ebdce5af2444ec9838a47856c3e
alonemanuel <alon@alonemanuel.com>
2023-03-01 09:08:04 +0200

    added

--- js/halom.js
+++ js/halom.js
@@ -1,13 +1,23 @@
+var placeholders = [];
+
 function setEventListeners() {
-    let ps = document.querySelectorAll('li > *');
-    for (p of ps) {
+    let ps = document.getElementsByTagName( 'li');
+    for (let p of ps) {
         p.addEventListener('click', () => {
+            let currPlaceholder = document.getElementById('placeholder');
+            if (currPlaceholder) {
+                p.parentNode.removeChild(currPlaceholder);
+            } else {
+                let placeholder = p.parentNode.insertBefore(document.createElement('div'), p);
+                placeholder.id = 'placeholder';
+                placeholder.innerText = ' ';
+
+            }
             console.debug('clicked');
-            p.classList.add('bigFont');
+            p.classList.toggle('bigFont');
             console.debug(p.classList);
         });
         
-        console.debug(p);
     }
 }
 
